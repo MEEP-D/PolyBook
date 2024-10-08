b405291bfb89a69c1c7ea932c5f94f1f5d051dc1
MEEP-D <acduong4567@gmail.com>
2024-10-08 20:35:13 +0700

    Part 4-2

---
+++ PolyBook/Repositories/HomeReponsitory.cs
@@ -0,0 +1,22 @@
+﻿using PolyBook.Data;
+using PolyBook.Models;
+
+namespace PolyBook.HomeReponsitory
+{
+    public class HomeReponsitory
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public HomeReponsitory(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        /*public async Task<IEnumerable<Book>> LayThongTinSachTyDb(string keySearch="",int theLoaiId =0 )
+        {
+            //chuyển đổi chuỗi sang dạng chữ thường
+            keySearch = keySearch.ToLower();
+            IEnumerable<Book> books = await ().ToListAsync();
+                        return books;
+        }*/
+    }
+}
