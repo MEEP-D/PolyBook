f7caf9c5784a6e59758e764b0cd2072d031a2347
MEEP-D <acduong4567@gmail.com>
2024-10-08 23:11:31 +0700

    Part-5-done

--- PolyBook/Controllers/HomeController.cs
+++ PolyBook/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using PolyBook.Models;
+using PolyBook.Models.DTOs;
 using PolyBook.Repositories;
 using System.Diagnostics;
 
@@ -18,17 +19,21 @@ namespace PolyBook.Controllers
         public async Task<IActionResult> Index(string keySearch = "", int theLooaiId = 0)
         {
             IEnumerable<Book> books = await _homeRepository.LayThongTinSachTuDb(keySearch, theLooaiId);
-            /*BookDislayModel bookDislaymodel = new BookDislayModel
-            { 
+            IEnumerable<Genre> genges = await _homeRepository.LayThongTinSachTuDb(keySearch, theLooaiId);
 
-            };*/
-            return View();
+            BookDislayModel bookDislaymodel = new BookDislayModel
+            {
+                Books = books,
+                Genres = genges,
+                keySearch = keySearch,
+            };
+            return View(books);
         }
 
-        public IActionResult Privacy()
+       /* public IActionResult Privacy()
         {
             return View();
-        }
+        }*/
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
